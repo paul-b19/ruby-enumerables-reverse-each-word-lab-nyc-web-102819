846e78a351eba539b1fada176cf4c82af8240be2
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-22 21:00:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,6 +4,6 @@ def reverse_each_word(str)
   p ar
   new_ar = ar.collect{|x| x.reverse}
   new_str = ""
-  ar.collect{|x| new_str += "#{x.reverse.to_s}"}
+  ar.collect{|x| new_str += "#{x.reverse.to_s}"}.join{
   p new_str
 end
