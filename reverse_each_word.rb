680b5c21a3cff4778ad91e567b4ed123ce092cb7
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-22 21:04:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(str)
   p str
   ar = str.split
   p ar
-  new_ar = ar.collect{|x| x.reverse}
-  #new_str = ""
-  ar.collect{|x| x.reverse.to_s}.join(" ")}
-  p ar
+  #new_ar = ar.collect{|x| x.reverse}
+  new_str = ""
+  new_str = ar.collect{|x| x.reverse.to_s}.join(" ")}
+  p new_str 
 end
